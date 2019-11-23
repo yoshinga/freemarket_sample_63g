766ac8d14f8a2f45a06803c830b5472db2cda30a
011yuu7003 <y0526.field@gmail.com>
2019-11-23 13:44:46 +0900

    自動デプロイ　修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@
 lock '3.11.2'
 
 # Capistranoのログの表示に利用する
-set :application, 'freemarket_sample_63g '
+set :application, 'freemarket_sample_63g'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:yoshinga/freemarket_sample_63g.git'
