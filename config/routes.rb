9fb36d8f61c20be2bc46df36260d420c0c7badce
yoshinga <y.tomo41@icloud.com>
2019-11-27 12:23:59 +0900

    create users_controller, users.html.haml

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
 
+  get 'users/index'
   root "view#index"
   devise_for :users 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
- 
+  resources :users, only: [:index, :edit, :update]
   resources :signup do
       collection do
         get 'registration'
