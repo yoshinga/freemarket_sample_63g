eafdeb618f65155db8019f6aec872229d52f4e8e
yoshinga <y.tomo41@icloud.com>
2019-11-27 13:08:28 +0900

    update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'users/index'
   root "view#index"
   devise_for :users 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
