809f0b7868d3fe4d6dde1b12d816322729aee864
011yuu7003 <y0526.field@gmail.com>
2019-11-27 10:30:49 +0900

    items routes　　
    viewを見るためにroutes, controllerに対して最低限の記述をしました

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root "view#index"
   devise_for :users 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
- 
+  resources :items, only: [:new]
   resources :signup do
       collection do
         get 'registration'
