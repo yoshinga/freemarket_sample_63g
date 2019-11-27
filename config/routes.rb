fcf9119a4fbb475d8fc4dfc91fc105117481648d
Naoki-Yuzu <toaru.programming@gmail.com>
2019-11-27 16:33:19 +0900

    fix indents

--- config/routes.rb
+++ config/routes.rb
@@ -5,17 +5,17 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:index, :edit, :update]
 
-  resources :mypages, only: [:index]
+  resources :mypages, only: [:index] 
  
   resources :logout, only: [:index]
 
   resources :signup do
-      collection do
-        get 'registration'
-        get 'sms_confirmation'
-        get 'address'
-        get 'credit_card' # ここで、入力の全てが終了する
-        get 'done' #入力完了後のページ
+    collection do
+      get 'registration'
+      get 'sms_confirmation'
+      get 'address'
+      get 'credit_card' # ここで、入力の全てが終了する
+      get 'done' #入力完了後のページ
     end
   end
 
