077c5370eebea582df0b7ba2a1b864ac774c1043
reremiyon <rtmiyon0825@gmail.com>
2019-12-02 19:12:03 +0900

    [WPI]delete-btn-server

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,16 @@
 class ItemsController < ApplicationController
-  
+
   def new
+  end
+  
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy 
+    redirect_to mypages_path
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   root "view#index"
   devise_for :users 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :items, only: [:new]
-  
+  resources :items, only: [:new, :show, :destroy]
+
   resources :users, only: [:index, :edit, :update]
 
   resources :mypages, only: [:index] do
