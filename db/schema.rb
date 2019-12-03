ac272e94ef2397ae70077ee13aadf2c3879da2da
Naoki-Yuzu <toaru.programming@gmail.com>
2019-12-03 15:09:23 +0900

    fix address table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-
 ActiveRecord::Schema.define(version: 2019_11_29_080946) do
-
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "postal_code", null: false
@@ -24,9 +21,7 @@ ActiveRecord::Schema.define(version: 2019_11_29_080946) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
-    
     t.index ["user_id"], name: "index_addresses_on_user_id"
-
   end
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -71,8 +66,6 @@ ActiveRecord::Schema.define(version: 2019_11_29_080946) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "images", "items"
-
   add_foreign_key "addresses", "users"
-
+  add_foreign_key "images", "items"
 end
