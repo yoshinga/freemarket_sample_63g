4de31b68d524bce90718ce4165c6c52b9815462b
reremiyon <rtmiyon0825@gmail.com>
2019-12-03 12:29:43 +0900

    例外処理実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,16 +1,26 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only:[:show]
 
   def new
   end
   
   def show
-    @item = Item.find(params[:id])
   end
 
   def destroy
-    item = Item.find(params[:id])
-    item.destroy 
+    begin
+      item = Item.find(params[:id])
+      item.destroy
+    rescue => e
+      Rails.logger.debug e.message
+    end
     redirect_to mypages_path
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
 end
