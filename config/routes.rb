86c258ea59c089f7173caf8a4ba0404f984b529b
reremiyon <rtmiyon0825@gmail.com>
2019-12-04 10:58:04 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :items, only: [:new, :create :show, :destroy]
+  resources :items, only: [:new, :create, :show, :destroy]
   
   resources :users, only: [:index, :edit, :update]
 
