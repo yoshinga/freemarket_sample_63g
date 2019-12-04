499fc0368b30223565964d0888efd75539fa2a12
seirotaniiwa <tyanyeeeeewa0912@icloud.com>
2019-12-04 11:47:55 +0900

    destroy-create-item-migrationfile

--- db/migrate/20191202074214_create_items.rb
+++
@@ -1,9 +0,0 @@
-class CreateItems < ActiveRecord::Migration[5.2]
-  def change
-    create_table :items do |t|
-      t.string     "item_name",    null: false, add_index: true
-      t.text       "discription",  null: false
-      t.references "user",         foreign_key: true
-    end
-  end
-end
