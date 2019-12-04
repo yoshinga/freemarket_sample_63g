4184348629aaba686ea1a5e172e16c665c947bd4
reremiyon <rtmiyon0825@gmail.com>
2019-12-04 11:56:33 +0900

    マイグレーションファイル複数削除

--- db/schema.rb
+++ db/schema.rb
@@ -42,14 +42,6 @@ ActiveRecord::Schema.define(version: 2019_11_29_080946) do
     t.integer "deliverydays_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-
-  end
-
-  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "item_name", null: false
-    t.text "discription", null: false
-    t.bigint "user_id"
-    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -74,11 +66,6 @@ ActiveRecord::Schema.define(version: 2019_11_29_080946) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-
-  add_foreign_key "items", "users"
-
   add_foreign_key "addresses", "users"
-
   add_foreign_key "images", "items"
-  
 end
