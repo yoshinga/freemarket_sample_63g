38eb26c3ab3bcb77cb402504d24765dd66fc57db
reremiyon <rtmiyon0825@gmail.com>
2019-12-05 12:03:34 +0900

    修正

--- db/schema.rb
+++ db/schema.rb
@@ -42,11 +42,6 @@ ActiveRecord::Schema.define(version: 2019_11_29_080946) do
     t.integer "deliverydays_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "discription", null: false
-    t.integer "burden_id", null: false
-    t.integer "prefecture_id", null: false
-    t.integer "condition_id", null: false
-    t.integer "price", null: false
     t.bigint "user_id"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
