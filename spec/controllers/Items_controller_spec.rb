4b4733aac2bae7d0e2f3aaab2b5c57e1293ec2ec
reremiyon <rtmiyon0825@gmail.com>
2019-12-06 18:04:53 +0900

    item-show-test

--- spec/controllers/Items_controller_spec.rb
+++ spec/controllers/Items_controller_spec.rb
@@ -7,10 +7,25 @@ describe ItemsController do
       delete :destroy, params: { id: item }
       expect(assigns(:item)).to eq item
     end 
-    it "redirect the :destroy template" do
+    it "redirect the :index template" do
       item = create(:item)
       delete :destroy, params: { id: item }
       expect(response).to redirect_to root_path
+    end
+  end
+
+  describe ItemsController do
+    describe 'GET #show' do
+      it "assigns the requested item to @item" do
+        item = create(:item)
+        get :show, params: { id: item }
+        expect(assigns(:item)).to eq item
+      end
+      it "renders the :show template" do
+        item = create(:item)
+        get :show, params: { id:item }
+        expect(response).to render_template :show
+      end
     end
   end
 end
