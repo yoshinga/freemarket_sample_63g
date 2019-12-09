04a8633eb77dcdf0b0be3ef7313343527a5dfd23
reremiyon <rtmiyon0825@gmail.com>
2019-12-09 18:03:24 +0900

    add-turbolinks:load

--- app/assets/javascripts/item-show.js
+++ app/assets/javascripts/item-show.js
@@ -1,4 +1,4 @@
-$(function(){
+$(document).on('turbolinks:load', function() {
   $("img.change-photo").hover(function(){
     var ImgSrc = $(this).attr("src");
     var ImgAlt = $(this).attr("alt");
