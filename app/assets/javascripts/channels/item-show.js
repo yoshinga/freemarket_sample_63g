fea9f623f763bed58467011f56148bc02bac66f0
reremiyon <rtmiyon0825@gmail.com>
2019-12-09 20:40:18 +0900

    修正

---
+++ app/assets/javascripts/channels/item-show.js
@@ -0,0 +1,10 @@
+$(function(){
+  $("img.change-photo").hover(function(){
+    var ImgSrc = $(this).attr("src");
+    var ImgAlt = $(this).attr("alt");
+    $("#main-photo").attr({src:ImgSrc,alt:ImgAlt});
+    $("#main-photo").hide();
+    $("#main-photo").fadeIn(0);
+    return false;
+  });
+});
