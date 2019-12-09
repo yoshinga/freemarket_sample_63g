04c0c722fb706b6dbbc4e2d3d28d3d02c9b627e2
011yuu7003 <y0526.field@gmail.com>
2019-12-09 21:19:38 +0900

    S3　本番環境

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,7 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  # if Rails.env.production? # 本番環境:AWS使用
+  if Rails.env.production? # 本番環境:AWS使用
     config.storage = :fog
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
@@ -14,9 +14,9 @@ CarrierWave.configure do |config|
     }
     config.fog_directory  = 'freemarket-sample-63g' #S3のバケット名
     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarket-sample-63g'
-  # else
-  #   config.storage :file # 開発環境:public/uploades下に保存
-  #   config.enable_processing = false if Rails.env.test? #test:処理をスキップ
-  # end  
+  else
+    config.storage :file # 開発環境:public/uploades下に保存
+    config.enable_processing = false if Rails.env.test? #test:処理をスキップ
+  end  
 
 end
