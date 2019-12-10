9a3a2ae8c9d51569a0e614db261bcde821784f57
yoshinga <y.tomo41@icloud.com>
2019-12-10 11:59:53 +0900

    update sns_credentials.rb

--- db/migrate/20191208003808_create_sns_credentials.rb
+++ db/migrate/20191208003808_create_sns_credentials.rb
@@ -2,7 +2,7 @@ class CreateSnsCredentials < ActiveRecord::Migration[5.2]
   def change
     create_table :sns_credentials do |t|
       t.string :provider
-      t.string :uid
+      t.string :uid, unique: true
       t.references :user
 
       t.timestamps
